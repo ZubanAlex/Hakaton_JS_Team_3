0604239ae96eed6abc78fbc24d23d000dde0b457
ZubanAlex <zubanalex31@gmail.com>
2019-11-24 17:28:50 +0200

    map complite

--- src/js/initMap.js
+++ src/js/initMap.js
@@ -1,12 +1,21 @@
 import StyleSettings from './mapStyleSettings.json';
 
-export default function initMap() {
+export default function initMap(locationArray, centr = [55.5555, 65.55555]) {
   let mapStyleSettings = StyleSettings;
   let options = {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    center: { lat: +centr[0], lng: +centr[1] },
+    zoom: 5,
     styles: mapStyleSettings,
     disableDefaultUI: true,
   };
+
   let map = new google.maps.Map(document.getElementById('map'), options);
+  locationArray.map(key => {
+    if (Array.isArray(key)) {
+      let marker = new google.maps.Marker({
+        position: { lat: key[0], lng: key[1] },
+        map: map,
+      });
+    }
+  });
 }
--- src/js/map.js
+++ src/js/map.js
@@ -1,14 +1,31 @@
 import '../css/map.css';
+import '../css/footer.css';
+import '../css/header.css';
 import initMap from './initMap';
 import createMapUserList from './renderMapUserList';
 import getUserForMap from './getUsersForMap';
 
 const KEY =
-  'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJwaG9uZV9udW1iZXIiOiJ1c2VyVGVsLnZhbHVlIiwiYWdlIjoidXNlckFnZS52YWx1ZSIsImlhdCI6MTU3NDUyNDMyMH0.CJcAiSm1lbnAwTOipBt-0KJI8ZPREWdRwRotDraRnoVYCPi3DVNhwXhhZDag36iV0cds7K5GnGheoCGf4sPLzQ';
+  'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJwaG9uZV9udW1iZXIiOiIyMzQ1Njc4IiwiYWdlIjoiMzMiLCJpYXQiOjE1NzQ1MzQ3MDl9.GEMn6_3wglzv6a5uqjuKqsqMS3aQYlfdOclONkxdmTKkqttTX1KPqZB1hPcIr1dblie_555RlydDYYgaeBaZeg';
 
 export default async function openMap() {
   let data = await getUserForMap(KEY);
-  initMap();
+  let geoLocation = data.map(key => key.geo_location);
+
+  const userList = document.querySelector('.mapUsers_list');
+
+  const getDataAttr = function(event) {
+    if (event.target.getAttribute('data-location')) {
+      initMap(
+        geoLocation,
+        event.target.getAttribute('data-location').split(','),
+      );
+    }
+  };
+
+  userList.addEventListener('click', getDataAttr);
+
+  initMap(geoLocation);
   createMapUserList(data);
-  console.log(data);
+  // console.log(data);
 }
