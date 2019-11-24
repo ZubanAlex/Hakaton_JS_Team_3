c66507244bef2517dab366d27aea2988623040cc
ZubanAlex <zubanalex31@gmail.com>
2019-11-24 18:20:33 +0200

    fix dev

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,3 @@
 import openMap from "./map";
 
 openMap();
-("use strict");
-
-import "swiper/css/swiper.min.css";
-import "../css/styles.css";
-import Swiper from "swiper";
-
-const mySwiper = new Swiper(".swiper-container", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination"
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar"
-  }
-});
